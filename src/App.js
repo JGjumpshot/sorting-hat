54127baefd4fd810c72e4ac9e9cf522cbff0280f
jgunther <jackson.gunther@yahoo.com>
2019-09-13 13:49:40 -0600

    working

--- src/App.js
+++ src/App.js
@@ -5,13 +5,23 @@ import './App.css';
 import './assets/sortinghat.png';
 import Title from './components/Title';
 import ArrowButton from './components/ArrowButton';
+import Test from './pages/Test';
 
 function App() {
   return (
-    <div className="background">
-      <Title />
-      <ArrowButton />
-    </div>
+    <Router>
+      <div className="background">
+        <Route exact path="/" component={Title}>
+          <Title />
+          <ArrowButton />
+        </Route>
+        <Route path="/test" component={Test}>
+          <Test />
+        </Route>
+      </div>
+
+    </Router>
+    
   );
 }
 
