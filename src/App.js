f6f3539c74af306e77a8250331b66ccc427c57b3
jacks <jackson.gunther@yahoo.com>
2019-11-18 21:45:53 -0700

    adding links

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {BrowserRouter as Router, Route, NavLink} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import './reset.css';
 import './App.css';
 import Title from './components/Title';
@@ -9,28 +9,42 @@ class App extends React.Component {
   constructor() {
     super()
     this.state = {
-        showMe:true
+      showMe: false
     }
   }
   componentWillUnmount() {
-      this.setState({
-          showMe:!this.state.showMe
-      })
+    this.setState({
+      showMe: !this.state.showMe
+    })
   }
+
+
 
   render() {
     return (
       <Router>
         <div className="background">
+          <Title />
+          <Link exact strict to="/">Home</Link>
+          <Link to="/greatHall">
+            {
+              <ArrowButton />           
+            }
+          </Link>
           <Route exact strict path="/" component={Title}></Route>
-          {
+          <Route exact strict path="/greatHall"></Route>
+          {/* <NavLink path="/test" component={Test}> */}
+          {/* {
             this.state.showMe ? <NavLink onClick={()=>this.componentWillUnmount()} className="button" exact strict to="/test" component={ArrowButton}>Let the sorting begin!</NavLink> : null
-          }
-                  
+          } */}
+          {/* </NavLink> */}
+
+
+
         </div>
-  
+
       </Router>
-      
+
     );
   }
 }
