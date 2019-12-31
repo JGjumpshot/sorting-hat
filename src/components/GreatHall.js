96e79dc95e13219022970649d19f60d1ad1119cd
jgunther <jackson.gunther@yahoo.com>
2019-12-31 14:25:21 -0700

    Discover House button start

--- src/components/GreatHall.js
+++ src/components/GreatHall.js
@@ -7,8 +7,8 @@ const GreatHall = () => {
         <div className="gold-svg"></div>
         <div className="hogwarts-crest"></div>
         <div className="castle"></div>
-      
-      {/* {document.getElementsByClassName('button').className = "hideButton"} */}
+        <div className="sorting-text">Are you Brave of heart? <br />&nbsp;&nbsp;Cunning and Ambitious? <br /> Loyal and Dedicated? <br /> &nbsp;&nbsp;Or Clever and Creative? <br /> <br /> Click to find out your house</div>
+        <button className="button-two">Discover House</button>
     </div>
   )}
 
