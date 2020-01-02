b605f130034bbd193cc09898753c3c93041ea7e3
jgunther <jackson.gunther@yahoo.com>
2020-01-02 14:48:24 -0700

    more changes

--- src/components/GreatHall.js
+++ src/components/GreatHall.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import './GreatHall.css';
 
-const GreatHall = () => {
+class GreatHall extends React.Component {
+  render() {
     return (
-    <div>
-        <div className="gold-svg"></div>
-        <div className="hogwarts-crest"></div>
-        <div className="castle"></div>
-        <div className="sorting-text">Are you Brave of heart? <br />&nbsp;&nbsp;Cunning and Ambitious? <br /> Loyal and Dedicated? <br /> &nbsp;&nbsp;Or Clever and Creative? <br /> <br /> Click to find out your house</div>
-        <button className="button-two">Discover House</button>
-    </div>
-  )}
+      <div>
+          <div className="gold-svg"></div>
+          <div className="hogwarts-crest"></div>
+          <div className="castle"></div>
+          <div className="sorting-text">Are you Brave of heart? <br />&nbsp;&nbsp;Cunning and Ambitious? <br /> Loyal and Dedicated? <br /> &nbsp;&nbsp;Or Clever and Creative? <br /> <br /> Click to find out your house</div>
+          <button className="button-two">Discover House</button>
+      </div>
+    );
+  }
+}
 
 export default GreatHall;
