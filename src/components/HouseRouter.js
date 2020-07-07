3e2b4ba8cedd7de25a0c707f79e014a371133f35
Jackson Gunther <jackson.gunther@yahoo.com>
2020-07-06 22:03:22 -0600

    houses router added

---
+++ src/components/HouseRouter.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function HouseRouter() {
+    return (
+        <div style={{"color": "white"}}>
+            Hello world
+        </div>
+    )
+}
+
+export default HouseRouter;
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,9 @@
 import React, { Component } from "react";
 import { Switch, Route } from "react-router-dom";
-
 //import history from './history';
 import Title from "./components/Title.js";
 import GreatHall from './components/GreatHall.js';
+import HouseRouter from './components/HouseRouter.js';
 export default class Routes extends Component {
     render() {
         return (
@@ -11,6 +11,7 @@ export default class Routes extends Component {
                 <Switch>
                     <Route exact path="/" component={Title} />
                     <Route path="/GreatHall" component={GreatHall} />
+                    <Route path="/HouseRouter" component={HouseRouter} />
                 </Switch>
             
         )
