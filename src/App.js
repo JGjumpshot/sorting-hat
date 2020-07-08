0d7415c39c9e7d65fc46ba34b1fe589a1ed100d2
Jackson Gunther <jackson.gunther@yahoo.com>
2020-07-07 22:21:09 -0600

    cleaned up app.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import React from "react";
-//import { BrowserRouter as Router, Route, Link} from 'react-router-dom';
 import "./reset.css";
 import "./App.css";
-//import Title from './components/Title';
 import "./components/Button.css";
-//import GreatHall from './components/GreatHall';
-//import history from './history';
 import Routes from "./routes";
 
 function App() {
-  // render(props) {
-  //   //if(this.state.isDisplayed === true) {
-
-  // }
 
   return (
     <div>
       <div className="background">
         <Routes />
-        {/* <Title /> */}
       </div>
     </div>
   );
