b9308f53433b39c1017267632708056ace72ed86
Jackson Gunther <jackson.gunther@yahoo.com>
2020-07-09 21:03:53 -0600

    creating house components

--- src/components/HouseRouter.js
+++ src/components/HouseRouter.js
@@ -1,9 +1,27 @@
 import React from 'react'
 
+function pickHouse() {
+    var houses = ['Gryffindor', 'Slytherin', 'Ravenclaw', 'Hufflepuff'];
+    var sorted = Math.floor(Math.random() * houses.length)
+    // console.log(houses[sorted]);
+    if (houses[sorted] === "Gryffindor") {
+        return "Gryffindor";
+    }
+    else if (houses[sorted] === "Slytherin") {
+        return "Slytherin";
+    }
+    else if (houses[sorted] === "Hufflepuff") {
+        return "Hufflepuff";
+    }
+    else {
+        return "Ravenclaw";
+    }
+}
+
 function HouseRouter() {
     return (
-        <div style={{"color": "white"}}>
-            Hello world
+        <div style={{"color": "white", "font-family": "monospace", "font-size": "200px"}}>
+            {pickHouse()}
         </div>
     )
 }
