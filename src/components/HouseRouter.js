aa7a8b279558d93a33ed2dc774f1dd5d9bafc5c5
Jackson Gunther <jackson.gunther@yahoo.com>
2020-07-24 21:02:55 -0600

    added HouseComponent

--- src/components/HouseRouter.js
+++ src/components/HouseRouter.js
@@ -1,20 +1,20 @@
 import React from 'react'
-
+import HouseComponent from './houses/HouseComponent.js';
 function pickHouse() {
     var houses = ['Gryffindor', 'Slytherin', 'Ravenclaw', 'Hufflepuff'];
     var sorted = Math.floor(Math.random() * houses.length)
     // console.log(houses[sorted]);
     if (houses[sorted] === "Gryffindor") {
-        return "Gryffindor";
+        return <HouseComponent house={houses[0]}/>
     }
     else if (houses[sorted] === "Slytherin") {
-        return "Slytherin";
+        return <HouseComponent house={houses[1]}/>
     }
     else if (houses[sorted] === "Hufflepuff") {
-        return "Hufflepuff";
+        return <HouseComponent house={houses[2]}/>
     }
     else {
-        return "Ravenclaw";
+        return <HouseComponent house={houses[3]}/>
     }
 }
 
---
+++ src/components/houses/HouseComponent.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function HouseComponent(props) {
+    return (
+        <div>
+            <h1 style={{color: "#444"}}>{props.house}</h1>
+        </div>
+    )
+}
+
+export default HouseComponent;
