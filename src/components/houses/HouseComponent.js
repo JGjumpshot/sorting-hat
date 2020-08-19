7541fba1ba3adb6c824001dd0204d2bd92e1efac
Jackson Gunther <jackson.gunther@yahoo.com>
2020-08-18 20:07:25 -0600

    added house name to HouseComponent

--- src/components/houses/HouseComponent.js
+++ src/components/houses/HouseComponent.js
@@ -2,10 +2,28 @@ import React from 'react'
 import './HouseComponent.css';
 
 function HouseComponent(props) {
+    
+    // switch (props.house) {
+    //     case "Hufflepuff":
+    //         house = "Hufflepuff";
+    //         break;
+    //     case "Gryffindor":
+    //         house = "Gryffindor";
+    //         break;
+    //     case "Slytherin":
+    //         house = "Slytherin";
+    //         break;
+    //     case "Ravenclaw":
+    //         house = "Ravenclaw";
+    //         break;
+    //     default:
+    //         break;
+    // }
     return (
         <div>
-            {/* <h1 className="house-styles">{props.house}</h1> */}
             <div className="gold-svg"></div>
+            <h1 className="house-styles"> Welcome to <span className="house-text">{props.house}</span></h1>
+            {/* <div className="house-styles">{props.house}</div> */}
             <div className="castle"></div>
         </div>
     )
