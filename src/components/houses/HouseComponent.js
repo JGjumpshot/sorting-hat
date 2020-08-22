307d23965bf5f669e4eb10405d94428bedfeb58e
Jackson Gunther <jackson.gunther@yahoo.com>
2020-08-22 13:24:14 -0600

    got logos working

--- src/components/houses/HouseComponent.js
+++ src/components/houses/HouseComponent.js
@@ -3,28 +3,31 @@ import './HouseComponent.css';
 
 function HouseComponent(props) {
     let house = props.house;
-    switch (house) {
-        case "Hufflepuff":
-            house = "Hufflepuff";
-            break;
-        case "Gryffindor":
-            house = "Gryffindor";
-            console.log(house);
-            return <div className="gryffindor-logo"></div>
-        case "Slytherin":
-            house = "Slytherin";
-            break;
-        case "Ravenclaw":
-            house = "Ravenclaw";
-            break;
-        default:
-            break;
+    function houseConditional(house) {
+        switch (house) {
+            case "Hufflepuff":
+                house = "Hufflepuff";
+                return <div className="gryffindor-logo"></div>
+            case "Gryffindor":
+                house = "Gryffindor";
+                return <div className="gryffindor-logo"></div>
+            case "Slytherin":
+                house = "Slytherin";
+                return <div className="gryffindor-logo"></div>
+            case "Ravenclaw":
+                house = "Ravenclaw";
+                return <div className="gryffindor-logo"></div>
+            default:
+                break;
+        }
     }
+    
     return (
         <div>
             <div className="gold-svg"></div>
             <h1 className="house-styles"> Welcome to <span className="house-text">{house}</span></h1>
             {/* <div className="house-styles">{props.house}</div> */}
+            {houseConditional(house)}
             <div className="castle"></div>
         </div>
     )
