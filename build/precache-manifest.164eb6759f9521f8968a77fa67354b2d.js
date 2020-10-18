164245edbdd5d2fec514d08ab60b6db1e88127b0
Jackson Gunther <jackson.gunther@yahoo.com>
2020-10-18 16:25:11 -0600

    added build folder

---
+++ build/precache-manifest.164eb6759f9521f8968a77fa67354b2d.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f23d85e23ddb70453deb603807932dde",
+    "url": "/sorting-hat/index.html"
+  },
+  {
+    "revision": "dbd4d49a166567722487",
+    "url": "/sorting-hat/static/css/main.0ddc516b.chunk.css"
+  },
+  {
+    "revision": "428a90c6d8abd2467ffc",
+    "url": "/sorting-hat/static/js/2.600cb0cb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/sorting-hat/static/js/2.600cb0cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dbd4d49a166567722487",
+    "url": "/sorting-hat/static/js/main.11cb5c5f.chunk.js"
+  },
+  {
+    "revision": "725dd66689ae58b31097",
+    "url": "/sorting-hat/static/js/runtime-main.c6027a19.js"
+  },
+  {
+    "revision": "61cf00b70cf72291e5300939d4610676",
+    "url": "/sorting-hat/static/media/Castle.61cf00b7.svg"
+  },
+  {
+    "revision": "181ef9a7aee45c119e68f294e8426a3a",
+    "url": "/sorting-hat/static/media/HARRYP__.181ef9a7.TTF"
+  },
+  {
+    "revision": "88f3c65970b802a193069a03430f4586",
+    "url": "/sorting-hat/static/media/button.88f3c659.svg"
+  },
+  {
+    "revision": "dc3bb0f4ce760f597cd61c7cd058bcaa",
+    "url": "/sorting-hat/static/media/goldOutline.dc3bb0f4.svg"
+  },
+  {
+    "revision": "9e1114a7ccb62dfa91738a486976cefc",
+    "url": "/sorting-hat/static/media/gryffindorLogo.9e1114a7.svg"
+  },
+  {
+    "revision": "e9b862e114d5b36a6dbd056ac3dcf835",
+    "url": "/sorting-hat/static/media/hogwartsLogo.e9b862e1.svg"
+  },
+  {
+    "revision": "e3eb53cbda85f97d0acc54e009959a83",
+    "url": "/sorting-hat/static/media/slytherin-logo.e3eb53cb.png"
+  },
+  {
+    "revision": "e111b1a36186b9ad70a74b80c706dd30",
+    "url": "/sorting-hat/static/media/sortinghatlogo.e111b1a3.png"
+  }
+]);
